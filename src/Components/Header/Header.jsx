4eae25aaa5f4cd719117a1ef71774bfa095034dc
AminaSultana08 <aminasultana088@gmail.com>
2023-09-12 15:41:24 +0600

    loaded fetched data

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+        <h1 className='text-4xl font-bold text-red-600'>Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
